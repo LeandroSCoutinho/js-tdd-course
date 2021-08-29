e7e83966451f207605028c79f09a5769df7220f1
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2021-08-28 23:27:00 -0300

    add git hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "plugin:react/recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
 };
